f2d704fa6052cef230fbd4ffd30b602429a7dc99
Neale Taylor <nealeptaylor@gmail.com>
2022-06-23 15:17:36 -0400

    playing around with parent child components

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-function TemporaryName() {
+function Header() {
   return (
-    <div>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </div>
+      <header>
+          <nav>
+              <img src="./react-logo.png" width="40px" />
+              
+          </nav>
+      </header>
   )
 }
 
-ReactDOM.render(<TemporaryName />, document.getElementById("root"))
+function Footer() {
+  return (
+      <footer>
+          <small>© 2021 Ziroll development. All rights reserved.</small>
+      </footer>
+  )
+}
+
+function MainContent() {
+  return (
+      <div>
+          <h1>Reasons I'm excited to learn React</h1>
+          <ol>
+              <li>It's a popular library, so I'll be 
+              able to fit in with the cool kids!</li>
+              <li>I'm more likely to get a job as a developer
+              if I know React</li>
+          </ol>
+      </div>
+  )
+}
+
+function Page() {
+  return (
+      <div>
+          <Header />
+          <MainContent />
+          <Footer />
+      </div>
+  )
+}
+
+
+ReactDOM.render(<Page />, document.getElementById("root"))

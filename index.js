a6040075ed5ec05c2c64bf7382b9f130c7c8d4c3
Neale Taylor <nealeptaylor@gmail.com>
2022-06-23 15:35:09 -0400

    move components into their own files

--- index.js
+++ index.js
@@ -1,49 +1,17 @@
-function Header() {
-  return (
-      <header>
-          <nav>
-              <img className="reactLogo" src="./react-logo.png" />
-              <ul className="nav-items">
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-          </nav>
-      </header>
-  )
-}
-
-function Footer() {
-  return (
-      <footer>
-          <small>© 2021 Ziroll development. All rights reserved.</small>
-      </footer>
-  )
-}
-
-function MainContent() {
-  return (
-      <div>
-          <h1>Reasons I'm excited to learn React</h1>
-          <ol>
-              <li>It's a popular library, so I'll be 
-              able to fit in with the cool kids!</li>
-              <li>I'm more likely to get a job as a developer
-              if I know React</li>
-          </ol>
-      </div>
-  )
-}
+import React from "react"
+import ReactDOM from "react-dom"
+import Header from "./Header"
+import MainContent from "./MainContent"
+import Footer from "./Footer"
 
 function Page() {
-  return (
-      <div>
-          <Header />
-          <MainContent />
-          <Footer />
-      </div>
-  )
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
 }
-
 
 ReactDOM.render(<Page />, document.getElementById("root"))
